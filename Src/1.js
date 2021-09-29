90f977c334af179e1514eccd2e1157553379c78b
yuran1811 <trieuvanbd123@gmail.com>
2021-09-29 09:26:00 +0700

    Update

--- Src/1.js
+++ Src/1.js
@@ -1,1 +1,42 @@
-const canvas = document.querySelector
+const canvas = document.querySelector('#draw')
+const ctx = canvas.getContext('2d')
+
+ctx.fillStyle = 'black'
+
+function draw(x, y)
+{
+	const circle = new Path2D();
+	circle.arc(x, y, 10, 0, 2 * Math.PI);
+	ctx.fill(circle)
+}
+
+let isDown = false;
+
+canvas.addEventListener('mouseup', (e) => {isDown = false;})
+canvas.addEventListener('mousedown', (e) =>
+{
+	isDown = true;
+	const {clientX, clientY} = e
+	const react = canvas.getBoundingClientRect()
+	draw(clientX - react.left, clientY - react.top)
+})
+canvas.addEventListener('mousemove', (e) =>
+{
+	if (!isDown) return
+	const {clientX, clientY} = e
+	const react = canvas.getBoundingClientRect()
+	draw(clientX - react.left, clientY - react.top)
+})
+
+const colorPickers = [...document.querySelectorAll('.color-picker')]
+colorPickers.forEach(colorPicker =>
+{
+	console.log('pick')
+	colorPicker.addEventListener('click', (e) =>
+	{
+		ctx.fillStyle = e.target.style.backgroundColor
+	})
+})
+
+const ButtonClear = document.querySelector('#btn-clear');
+ButtonClear.addEventListener('click', (e) => {ctx.clearRect(0, 0, 600, 600)})
