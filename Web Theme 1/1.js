c0c3e06c45e0abf595e0f68a470aac8afc6f60e3
Yuran <76398420+yuran1811@users.noreply.github.com>
2021-10-17 09:17:02 +0700

    Update 1.js

--- Web Theme 1/1.js
+++ Web Theme 1/1.js
@@ -1,3 +1,7 @@
+const hamburgerMenu = document.querySelector(".hamburger-menu");
+const navbarContainer = document.querySelector(".navbar-container");
+hamburgerMenu.addEventListener("click", () => {navbarContainer.classList.toggle("active");})
+
 const SocialPanel_container = document.querySelector('.social-panel-container');
 const FloatingBtn = document.querySelector('.floating-btn');
 const ToggleBtn = document.querySelector('#toggle-btn');
