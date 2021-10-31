1ad24f8d3641e87b586656513fea63cc60f15a3a
Yuran <76398420+yuran1811@users.noreply.github.com>
2021-10-31 07:55:37 +0700

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,21 @@
 const menuToggle = document.querySelector(".menu");
-const mainContainer = document.querySelector(".main");
 const labelToggle = document.querySelector('.label');
+const mainContainer = document.querySelector(".main");
 const navbarContainer = document.querySelector(".navbar-container");
 menuToggle.addEventListener("click", () => {
 	navbarContainer.classList.toggle("active");
 	mainContainer.classList.toggle("active");
 	labelToggle.classList.toggle("active");
 	menuToggle.classList.toggle("active");
+
+	var linkList = document.querySelector(".links").getElementsByClassName("lk");
+	for (var i = 0; i < linkList.length; i++) {
+		linkList[i].addEventListener("click", function() {
+			var current = document.getElementsByClassName("liactive");
+			current[0].className = current[0].className.replace(" liactive", "");
+			this.className += " liactive";
+		});
+	}
 })
 
 const ToggleBtn = document.querySelector('#toggle-btn');
