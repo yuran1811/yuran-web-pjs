1d865dd18f88a48fa5614e45decabf7ab8191f09
yuran1811 <trieuvanbd123@gmail.com>
2021-11-06 10:54:00 +0700

    Update

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,24 @@
 const menuToggle = document.querySelector(".menu");
-const labelToggle = document.querySelector('.label');
+const labelToggle = document.querySelector(".label");
 const mainContainer = document.querySelector(".main");
 const navbarContainer = document.querySelector(".navbar-container");
 menuToggle.addEventListener("click", () => {
-	navbarContainer.classList.toggle("active");
-	mainContainer.classList.toggle("active");
-	labelToggle.classList.toggle("active");
-	menuToggle.classList.toggle("active");
+  navbarContainer.classList.toggle("active");
+  mainContainer.classList.toggle("active");
+  labelToggle.classList.toggle("active");
+  menuToggle.classList.toggle("active");
 
-	var linkList = document.querySelector(".links").getElementsByClassName("lk");
-	for (var i = 0; i < linkList.length; i++) {
-		linkList[i].addEventListener("click", function() {
-			var current = document.getElementsByClassName("liactive");
-			current[0].className = current[0].className.replace(" liactive", "");
-			this.className += " liactive";
-		});
-	}
-})
+  var linkList = document.querySelector(".links").getElementsByClassName("lk");
+  for (var i = 0; i < linkList.length; i++) {
+    linkList[i].addEventListener("click", function () {
+      var current = document.getElementsByClassName("liactive");
+      current[0].className = current[0].className.replace(" liactive", "");
+      this.className += " liactive";
+    });
+  }
+});
 
-const ToggleBtn = document.querySelector('#toggle-btn');
-ToggleBtn.addEventListener('change', (e) => {document.body.classList.toggle('dark');});
+const ToggleBtn = document.querySelector("#toggle-btn");
+ToggleBtn.addEventListener("change", (e) => {
+  document.body.classList.toggle("dark");
+});
