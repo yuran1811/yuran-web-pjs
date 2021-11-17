a34e9d884877afccafbf7946ab90c3425060f77f
yuran1811 <trieuvanbd123@gmail.com>
2021-11-17 19:07:16 +0700

    Update

--- src/js/main.js
+++ src/js/main.js
@@ -1,57 +1,40 @@
 const menuToggle = document.querySelector('.menu');
-const labelToggle = document.querySelector('.label');
 const mainContainer = document.querySelector('.main');
 const navbarContainer = document.querySelector('.navbar-container');
-menuToggle.addEventListener('click', () => {
-	navbarContainer.classList.toggle('active');
-	mainContainer.classList.toggle('active');
-	labelToggle.classList.toggle('active');
-	menuToggle.classList.toggle('active');
 
-	let linkList = document.querySelectorAll('.links .lk');
-	console.log(typeof linkList);
-	for (var link of linkList)
+(() => {
+	const linkList = document.querySelectorAll('.links .lk');
+	for (let link of linkList)
 		link.addEventListener('click', (e) => {
 			let last = document.querySelector('.liactive');
 			last.className = last.className.replace(' liactive', '');
 			e.path[1].className += ' liactive';
 		});
-});
+})();
 
-const ToggleBtn = document.querySelector('#toggle-btn');
-ToggleBtn.addEventListener('change', () => {
+function toggleActive() {
+	(() => {
+		navbarContainer.classList.toggle('active');
+		mainContainer.classList.toggle('active');
+		menuToggle.classList.toggle('active');
+	})();
+}
+
+document.querySelector('#toggle-btn').addEventListener('change', () => {
 	document.body.classList.toggle('dark');
 });
 
-function adjustMedia() {
-	let linkList = document.querySelectorAll('.links .lk');
-	for (var link of linkList)
-		link.addEventListener('click', (e) => {
-			let last = document.querySelector('.liactive');
-			last.className = last.className.replace(' liactive', '');
-			e.path[1].className += ' liactive';
-
-			navbarContainer.classList.toggle('active');
-			mainContainer.classList.toggle('active');
-			labelToggle.classList.toggle('active');
-			menuToggle.classList.toggle('active');
-		});
-}
-
-let isMatched = false;
-let isAdjust = false;
-
-const mediaQueryList = window.matchMedia('(max-width: 640px)');
-mediaQueryList.addListener((e) => {
-	if (e.matches && !isMatched && !isAdjust) {
-		adjustMedia();
-		isMatched = true;
-	}
-});
+menuToggle.addEventListener('click', toggleActive);
 
 setInterval(() => {
-	if (window.innerWidth <= 640 && !isAdjust && !isMatched) {
-		adjustMedia();
-		isAdjust = true;
+	if (window.innerWidth <= 640) {
+		if (menuToggle.classList.contains('active'))
+			document
+				.querySelector('.links')
+				.addEventListener('click', toggleActive);
+		else
+			document
+				.querySelector('.links')
+				.removeEventListener('click', toggleActive);
 	}
-}, 1000);
+}, 500);
