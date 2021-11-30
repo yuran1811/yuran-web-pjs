72b9eceb147e53842af949736f3ba28174ab2892
yuran1811 <trieuvanbd123@gmail.com>
2021-11-30 15:06:18 +0700

    Update

--- Web Theme/Web Theme 1/1.js
+++ Web Theme/Web Theme 1/1.js
@@ -1,60 +1,64 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
-const closeBtn = $('.close-btn');
-const floatBtn = $('.floating-btn');
-const socialPanelConatainer = $('.social-panel-container');
-
-floatBtn.addEventListener('mouseover', () => {
-	socialPanelConatainer.classList.toggle('visible');
-});
-floatBtn.addEventListener('mouseout', () => {
-	socialPanelConatainer.classList.remove('visible');
-});
-
-socialPanelConatainer.addEventListener('mouseover', () => {
-	socialPanelConatainer.classList.toggle('visible');
-});
-socialPanelConatainer.addEventListener('mouseout', () => {
-	socialPanelConatainer.classList.remove('visible');
-});
-
-closeBtn.addEventListener('click', () => {
-	socialPanelConatainer.classList.remove('visible');
-});
-
-const menuToggle = $('.menu');
-const mainContainer = $('.main');
-const navbarContainer = $('.navbar-container');
-
 (() => {
-	$$('.links .lk').map((item) => {
-		item.addEventListener('click', (e) => {
-			const last = $('.liactive');
-			last.className = last.className.replace(' liactive', '');
-			e.path[1].className += ' liactive';
-		});
+	const closeBtn = $('.close-btn');
+	const floatBtn = $('.floating-btn');
+	const socialPanelConatainer = $('.social-panel-container');
+
+	floatBtn.addEventListener('mouseover', () => {
+		socialPanelConatainer.classList.toggle('visible');
+	});
+	floatBtn.addEventListener('mouseout', () => {
+		socialPanelConatainer.classList.remove('visible');
+	});
+
+	socialPanelConatainer.addEventListener('mouseover', () => {
+		socialPanelConatainer.classList.toggle('visible');
+	});
+	socialPanelConatainer.addEventListener('mouseout', () => {
+		socialPanelConatainer.classList.remove('visible');
+	});
+
+	closeBtn.addEventListener('click', () => {
+		socialPanelConatainer.classList.remove('visible');
 	});
 })();
 
-function toggleActive() {
+(() => {
+	const menuToggle = $('.menu');
+	const mainContainer = $('.main');
+	const navbarContainer = $('.navbar-container');
+
 	(() => {
-		navbarContainer.classList.toggle('active');
-		mainContainer.classList.toggle('active');
-		menuToggle.classList.toggle('active');
+		const linkList = $$('.links .lk');
+		for (let link of linkList)
+			link.addEventListener('click', (e) => {
+				const last = $('.liactive');
+				last.className = last.className.replace(' liactive', '');
+				e.path[1].className += ' liactive';
+			});
 	})();
-}
 
-$('#toggle-btn').addEventListener('change', () => {
-	document.body.classList.toggle('dark');
-});
-
-menuToggle.addEventListener('click', toggleActive);
-
-setInterval(() => {
-	if (window.innerWidth <= 640) {
-		if (menuToggle.classList.contains('active'))
-			$('.links').addEventListener('click', toggleActive);
-		else $('.links').removeEventListener('click', toggleActive);
+	function toggleActive() {
+		(() => {
+			navbarContainer.classList.toggle('active');
+			mainContainer.classList.toggle('active');
+			menuToggle.classList.toggle('active');
+		})();
 	}
-}, 500);
+
+	$('#toggle-btn').addEventListener('change', () => {
+		document.body.classList.toggle('dark');
+	});
+
+	menuToggle.addEventListener('click', toggleActive);
+
+	setInterval(() => {
+		if (window.innerWidth <= 640) {
+			if (menuToggle.classList.contains('active'))
+				$('.links').addEventListener('click', toggleActive);
+			else $('.links').removeEventListener('click', toggleActive);
+		}
+	}, 500);
+})();
